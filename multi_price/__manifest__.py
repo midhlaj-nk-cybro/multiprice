412f7e9102c65c6113fe0388e24855cb47aaf923
Midhlaj Nechikkandan <midhlaj.nk@gmail.com>
2024-10-18 18:55:10 +0530

    test

---
+++ multi_price/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Multi price',
+    'version': '17.0.1.0.1',
+    'description': 'Multiple price for single product',
+    'summary': """Multiple price for single product based on company brach""",
+    # 'data': [
+    #     ],
+    'depends': ['base','product'],
+    'installable': True,
+    'license': 'OPL-1',
+    'auto_install': False,
+    'application': False,
+}
